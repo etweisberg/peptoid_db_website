a831f3596fb26fb80d690ae7d7ef82f3d58d0296
etweisberg <58375851+etweisberg@users.noreply.github.com>
2020-05-13 16:00:17 -0400

    building up html templates

--- databank/app/routes.py
+++ databank/app/routes.py
@@ -1,6 +1,12 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    peptoid = {
+    'name':'19AB1-2-A',
+    'experiment':'X-ray diffraction',
+    'authors':'Castellano, V.; Chippindale, A.M.; Hamley, I.W.; Barnett, S.; Hasan, A.; Lau, K.H.A.'
+    }
+    return render_template('index.html',title = 'Home',peptoid = peptoid)
