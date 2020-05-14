b91916df59ce5eb8aa87025aef1e235e4016ab9d
etweisberg <58375851+etweisberg@users.noreply.github.com>
2020-05-14 17:48:11 -0400

    fixing flask shell

--- databank/databank.py
+++ databank/databank.py
@@ -1,6 +1,6 @@
-from app import app
+from app import app, db
 from app.models import Peptoid
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'Peptoid': User}
+    return {'db': db, 'Peptoid': Peptoid}
