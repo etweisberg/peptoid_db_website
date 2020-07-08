27777d0c835d71e5e3c0215e73740f259903d077
etweisberg <58375851+etweisberg@users.noreply.github.com>
2020-07-08 15:48:33 -0400

    completed search and filters

--- databank/app/forms.py
+++ databank/app/forms.py
@@ -1,8 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, RadioField
 from wtforms.validators import DataRequired
 
 #making form for searching data bank
 class SearchForm(FlaskForm):
+    option = RadioField('Select an option for searching the peptoid data bank', choices=[
+        ('residue','Peptoid Residue'),
+        ('author','Name of Author'),
+        ('experiment','Experimental Technique'),
+        ('doi','DOI'),
+        ])
     search = StringField('Enter search terms', validators=[DataRequired()])
     submit = SubmitField('Go')
