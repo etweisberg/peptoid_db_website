2ee450d14d2753ec085a8c891b03cbc072a4e01e
etweisberg <58375851+etweisberg@users.noreply.github.com>
2020-07-13 17:48:50 -0400

    docker modification;

--- databank/boot.sh
+++ databank/boot.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 source venv/bin/activate
-flask db upgrade
+while true; do
+    flask db upgrade
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Upgrade command failed, retrying in 5 secs...
+    sleep 5
+done
 python db_add_test.py
 exec gunicorn -b :5000 --access-logfile - --error-logfile - databank:app
