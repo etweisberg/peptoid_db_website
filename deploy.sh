61db8551d4c99edbe34c4e9d75e7fe2631c5b984
etweisberg <58375851+etweisberg@users.noreply.github.com>
2020-07-31 16:15:31 -0400

    trying to set up travis ci

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+set -e
+
+IMAGE="etweisberg/databank"
+GIT_VERSION=$(git describe --always --abbrev --tags --long)
+
+docker build -d ${IMAGE}:${GIT_VERSION} databank
+docker tag ${IMAGE}:${GIT_VERSION} ${IMAGE}:latest
+
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+docker push ${IMAGE}:${GIT_VERSION}
+
+#ssh commands
+ssh -i ${SSH_KEY} root@159.89.232.85
+
+docker stop current-container
+docker rm current-container
+docker run --name=current-container --restart unless-stopped -d -p 80:5000 ${IMAGE}:${GIT_VERSION}
+docker system prune -a -f
